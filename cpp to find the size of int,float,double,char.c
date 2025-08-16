8bf32ab440ce51607825b165597335fbb7408482
nusrat329 <audrinusrat@gmail.com>
2025-08-16 12:44:23 -0700

    Add files via upload

---
+++ cpp to find the size of int,float,double,char.c
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main (){
+ cout<<"size of int:" <<sizeof (int) <<"bytes"<<end1;
+cout<<"size of float:" <<sizeof (float) <<"bytes"<<end1;
+cout<<"size of double:" <<sizeof (double) <<"bytes"<<end1;
+cout<<"size of char:" <<sizeof (char) <<"bytes"<<end1;
+return 0;
+}
