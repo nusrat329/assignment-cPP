9c3d89cd49ce3ce93ac87d9005a2eac0f56fce0c
nusrat329 <audrinusrat@gmail.com>
2025-08-16 12:15:54 -0700

    Add files via upload

---
+++ c++program to multiply two floating point numbers.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+float num1 , num2, product;
+cout <<"Enter first number:";
+cin >> num1;
+cout <<"Enter second number:";
+cin >> num2;
+product = num1 * num2 ;
+cout <<"Produt = " <<product <<end1;
+return 0;
+}
