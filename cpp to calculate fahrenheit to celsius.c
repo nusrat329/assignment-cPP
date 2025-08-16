1a365d0e831b16b298d1dadecd1a13d5b236bf4b
nusrat329 <audrinusrat@gmail.com>
2025-08-16 12:36:41 -0700

    Add files via upload

---
+++ cpp to calculate fahrenheit to celsius.c
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+float fahrenheit , celsius ;
+cout <<"Enter temperature in fahrenheit:";
+cin >> fahrenheit;
+celsius = (fahrenheit - 32)*5/9;
+cout <<"Temperature in celsius: " << celsius "<<end1;
+return 0;
+}
