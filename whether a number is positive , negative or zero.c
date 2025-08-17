33f070c84380677b07167d396161306062c726f2
nusrat329 <audrinusrat@gmail.com>
2025-08-16 23:54:50 -0700

    Add files via upload

---
+++ whether a number is positive , negative or zero.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int num;
+ cout << "Enter a number : " ;
+ cin >> num;
+   if(num > 0)
+ cout << num << "is positive. " << end1;
+   else if(num < 0)
+ cout << num << "is negative. " << end1;
+    else
+ cout << "The number is zero. "  << end1;
+ return 0;
+ }
