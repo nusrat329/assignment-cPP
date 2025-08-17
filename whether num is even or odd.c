9132a0a5943dc7fa854b5acda4bdd75996ea91f5
nusrat329 <audrinusrat@gmail.com>
2025-08-16 23:59:28 -0700

    Add files via upload

---
+++ whether num is even or odd.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int num;
+ cout << "Enter a number : " ;
+ cin >> num;
+   if(num % 2 == 0)
+ cout << num << "is even. " << end1;
+  else
+ cout << num << "is odd. " << end1;
+ return 0;
+ }
+
+
