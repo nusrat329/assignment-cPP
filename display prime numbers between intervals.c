6e284059752ea5da28d3c6f0733781ad8ab73a00
nusrat329 <audrinusrat@gmail.com>
2025-08-17 04:27:48 -0700

    Add files via upload

---
+++ display prime numbers between intervals.c
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int low , high,i, flag;
+
+  cout <<"Enter two numbers(intervals): ";
+  cin >> loow >> high;
+  cout <<"Prime numbers between" << low <<"and " <<high <<"are:" ;
+  for (int num = low; num <= high; num++) {
+   if (num <= 1 )
+   continue;
+   flag = 1 ;
+
+   for (i = 2; i*i <= num; i++) {
+   if (num % i == 0) {
+    flag = 0;
+    break;
+   }
+
+}
+if (flag == 1)
+cout << num << " ";
+  }
+return 0;
+
+}
