ec4ce45616098e2bef88f67ab4b33c867bdfde90
nusrat329 <audrinusrat@gmail.com>
2025-08-16 23:37:37 -0700

    Add files via upload

---
+++ find simple interest.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+ float principal , rate , time , simple interest;
+ cout << "Enter principal amount: " ;
+ cin >> Principal;
+ cout << "Enter rate of interest (%): " ;
+ cin >> rate;
+ cout << "Enter time (in years): " ;
+ cin >> time;
+   simpleinterest =(principal * rate* time)/100;
+cout <<"Simple interest = " << simpleinterest << end1 ;
+return 0;
+}
