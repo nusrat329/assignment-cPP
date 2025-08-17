6a1b774ef04a21d4db49cc7a59118e3fa0e64460
nusrat329 <audrinusrat@gmail.com>
2025-08-16 23:49:43 -0700

    Add files via upload

---
+++ area and perimeter of rectangle.c
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+ float length , width , area , perimeter;
+ cout << "Enter length of the rectangle: " ;
+ cin >> length;
+ cout << "Enter width of the rectangle: " ;
+ cin >> width;
+
+ area = length * width ;
+ perimeter = 2 * (length + width);
+ cout <<"Area of rectangle = " <<area << end1;
+ cout <<"Perimeter of rectangle = " << perimeter << end1;
+ return 0;
+ }
