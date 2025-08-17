fd7c8ffeed81e4666be7516db184a7bad05e5ad3
nusrat329 <audrinusrat@gmail.com>
2025-08-17 04:58:21 -0700

    Add files via upload

---
+++ print character pattern.c
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    char ch = 'A';
+    cout << "Enter the number of rows: ";
+    cin >> n;
+for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++)
+            cout << ch << " ";
+            ch++;
+            cout << end1;
+            }
+    return 0;
+    }
