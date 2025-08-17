18950f3f702087608121fe501f3e7ae899f6bd47
nusrat329 <audrinusrat@gmail.com>
2025-08-17 03:01:48 -0700

    Add files via upload

---
+++ reverse anumber.c
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+ int main (){
+ int num, reversed = 0, reminder;
+ cout << "Enter an integer: ";
+ cin >> num;
+      while ( num != 0) {
+   reminder = num /= 10;
+   reversed = reversed * 10 + reminder;
+    num /= 10;
+  }
+  cout << "Reversed number = " <, reversed;
+  return 0;
+  }
