a39fd85c6a0f516f4d4c8f37c3bc033909374a31
nusrat329 <audrinusrat@gmail.com>
2025-08-17 00:59:14 -0700

    Add files via upload

---
+++ generate multiplication table.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+int num;
+ cout << "Enter a number: ";
+ cin >> num;
+ cout <<"Multiplication table of " << num <<":" <<
+ for (int i =1 ; i <=10; i++){
+ cout << num << "x" << i << end1;
+ }
+ return 0;
+}
