44520883cc19d54fabbef49245bc30f85d9d5879
nusrat329 <audrinusrat@gmail.com>
2025-08-17 00:26:18 -0700

    Add files via upload

---
+++ print alphabets from A to Z using loop.c
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespaqce std ;
+int main () {
+ cout << "Alphabets from A to Z are: ";
+  for(char ch= 'A'; chh <= 'Z'; ch++) {
+   cout <<  ch <<" "
+  }
+   cout << end1;
+   return 0;
+   }
