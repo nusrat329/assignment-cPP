a8a06d20230dc3852cabe022438f73dff0a4537b
nusrat329 <audrinusrat@gmail.com>
2025-08-17 04:38:48 -0700

    Add files via upload

---
+++ find all factors of a natural number.c
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int num;
+  cout << "Enter a positive integer: ";
+  cin >> num;
+  cout <, "Factors of " << num <<"are: ";
+  for (int i = 1; i<= num; i++) {
+     if (num % i == 0)
+     cout << i << " ";
+  }
+    return 0;
+    }
