23c95275e51cac6c642d3a728d60621766b35d95
nusrat329 <audrinusrat@gmail.com>
2025-08-22 01:15:49 -0700

    Add files via upload

---
+++ find all roots of a quadratic equation.c
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    double a, b, c;
+
+    cout << "Enter coefficients a, b, and c: ";
+    cin >> a >> b >> c;
+
+    if (a == 0) {
+        cout << "This is not a quadratic equation." << endl;
+        return 1;
+    }
+
+    double discriminant = b * b - 4 * a * c;
+    cout << fixed;
+    if (discriminant > 0) {
+        double root1 = (-b + sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - sqrt(discriminant)) / (2 * a);
+        cout << "Roots are real and different." << endl;
+        cout << "Root 1 = " << root1 << endl;
+        cout << "Root 2 = " << root2 << endl;
+    } else if (discriminant == 0) {
+        double root = -b / (2 * a);
+        cout << "Roots are real and equal." << endl;
+        cout << "Root = " << root << endl;
+    } else {
+        double realPart = -b / (2 * a);
+        double imagPart = sqrt(-discriminant) / (2 * a);
+        cout << "Roots are complex and different." << endl;
+        cout << "Root 1 = " << realPart << " + " << imagPart << "i" << endl;
+        cout << "Root 2 = " << realPart << " - " << imagPart << "i" << endl;
+    }
+
+    return 0;
+}
