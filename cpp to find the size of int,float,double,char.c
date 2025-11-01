699a8acf5206f7b704618eecadd2a71e6d026cae
nusrat329 <audrinusrat@gmail.com>
2025-10-31 22:32:04 -0700

    Update cpp to find the size of int,float,double,char.cpp

--- cpp to find the size of int,float,double,char.c
+++ cpp to find the size of int,float,double,char.c
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-int main (){
- cout<<"size of int:" <<sizeof (int) <<"bytes"<<end1;
-cout<<"size of float:" <<sizeof (float) <<"bytes"<<end1;
-cout<<"size of double:" <<sizeof (double) <<"bytes"<<end1;
-cout<<"size of char:" <<sizeof (char) <<"bytes"<<end1;
+int main() {
+    cout << "int: " << sizeof(int) << endl;
+    cout << "float: " << sizeof(float) << endl;
+    cout << "double: " << sizeof(double) << endl;
+    cout << "char: " << sizeof(char);
 return 0;
 }
