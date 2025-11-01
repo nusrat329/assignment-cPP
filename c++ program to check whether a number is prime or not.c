1425324a8796c726842c0235474bc9efa917279e
nusrat329 <audrinusrat@gmail.com>
2025-10-31 22:09:54 -0700

    Update c++ program to check whether a number is prime or not.cpp

--- c++ program to check whether a number is prime or not.c
+++ c++ program to check whether a number is prime or not.c
@@ -1,23 +1,12 @@
-#include <iostream>
+#include <iostream>
 using namespace std;
 int main() {
-     int num , i;
-     bool is Prime = true;
-     cout<<"Enter a number :";
-     cin>> num;
-     if(num <= 1){
-      is Prime = false;
-     } else {
-     for(i=2;i*i <= num ; i++) {
-     if (num % i== 0) {
-     is Prime = false;
-     break;
-     }
-     }
-     }
-     if (is Prime)
-     cout<<num << "is a prime number . " <<end1;
-     else
-     cout <<num <<sum <<" is not a prime number ." <<end1 ;
-     return 0 ;
-     }
+    int n = 7, i;
+    bool prime = true;
+    for(i=2; i<=n/2; i++)
+        if(n%i==0) prime = false;
+    if(prime) cout << "Prime";
+    else 
+cout << "Not Prime";
+return 0;
+}
