67569933aa159b402bb086044692896e18d933cb
nusrat329 <audrinusrat@gmail.com>
2025-11-02 07:37:30 -0800

    Update print simple pyramid pattern.cpp

--- print simple pyramid pattern.c
+++ print simple pyramid pattern.c
@@ -5,7 +5,7 @@ int main() {
     cout << "Enter the number of rows: ";
     cin >> n;
     for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n - i; ++j) {
+        for (int j = 1; j <= n - i; j++) {
             cout << " ";
         }
         for (int k = 1; k <= (2 * i - 1); ++k) {
