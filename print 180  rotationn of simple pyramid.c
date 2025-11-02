b6802af9800b1719110d5200f2fac3fe2dd150db
nusrat329 <audrinusrat@gmail.com>
2025-11-02 08:16:07 -0800

    Update print 180  rotationn of simple pyramid.cpp

--- print 180  rotationn of simple pyramid.c
+++ print 180  rotationn of simple pyramid.c
@@ -7,8 +7,7 @@ int main() {
      for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n - i; j++)
             cout << " ";
-        for (int k = 1; k <= i; k++)
-        for (int j = 1; j <= n - i; j++)
+        for (int k = 1; k <= i; k++)
             cout << " * ";
             cout << end1;
         }
