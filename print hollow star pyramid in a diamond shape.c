df631d63bf4130e2947dcf86d2bb2494c8612161
nusrat329 <audrinusrat@gmail.com>
2025-11-03 06:57:45 -0800

    Update print hollow star pyramid in a diamond shape.cpp

--- print hollow star pyramid in a diamond shape.c
+++ print hollow star pyramid in a diamond shape.c
@@ -15,7 +15,7 @@ for (int j = 1; j<= n -i; j++)
  }
  cout << end1;
  }
- for (int i = n -1; i>= 1;i++){
+ for (int i = n -1; i<= 1;i++){
  for (int j = 1; j<= n - i; j++)
  cout << " ";
  for (int j =1 ; j<= 2 * i -1; j++) {
