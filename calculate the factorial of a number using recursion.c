ebe34ad5e8593aacef2db6cbf3fa0207a2dc1b37
nusrat329 <audrinusrat@gmail.com>
2025-11-04 07:34:10 -0800

    Update calculate the factorial of a number using recursion.cpp

--- calculate the factorial of a number using recursion.c
+++ calculate the factorial of a number using recursion.c
@@ -9,22 +9,8 @@ int factorial(int n) {
 int main() {
     int n;
     cout << "Enter a non-negative integer: ";
-    cin >> n;#include <iostream>
-using namespace std;
-
-int factorial(int n) {
-    if (n <= 1)
-        return 1;
-    else
-        return n * factorial(n - 1);
-}
-
-int main() {
-    int n;
-    cout << "Enter a non-negative integer: ";
     cin >> n;
-
-    if (n < 0)
+if (n < 0)
         cout << "Factorial is not defined for negative numbers." << endl;
     else
         cout << "Factorial of " << n << " is: " << factorial(n) << endl;
