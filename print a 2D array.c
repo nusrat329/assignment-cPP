6bc7bd0d8a5560bd26fcf901101a016b88d064b5
nusrat329 <audrinusrat@gmail.com>
2025-11-07 05:11:05 -0800

    Update print a 2D array.cpp

--- print a 2D array.c
+++ print a 2D array.c
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-    const int rows = 3;
-    const int columns = 4;
-    int array[rows][columns] = {
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12}
-    };
-    cout << "2D Array contents:\n";
+    int rows, cols;
+    cout << "Enter rows and columns: ";
+    cin >> rows >> cols;
+    int arr[rows][cols];
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < cols; j++)
+            cin >> arr[i][j];
+
+    cout << "2D Array:\n";
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            cout << array[i][j] << " ";
-        }
+        for (int j = 0; j < cols; j++)
+            cout << arr[i][j] << " ";
         cout << endl;
     }
-
     return 0;
 }
