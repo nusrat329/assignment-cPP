48fdd81ee1ded0362dc01dadf436430c32a7d131
nusrat329 <audrinusrat@gmail.com>
2025-11-07 05:21:21 -0800

    Update find the largest element in an array.cpp

--- find the largest element in an array.c
+++ find the largest element in an array.c
@@ -9,7 +9,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    int max = arr[0;
+    int max = arr[0];
     for (int i = 1; i < n; i++) {
         if (arr[i] > max) {
             max = arr[i];
