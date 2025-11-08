aa5e6bb9e28e977f2b043a279d484e7b98078844
nusrat329 <audrinusrat@gmail.com>
2025-11-08 05:17:17 -0800

    Update cpp to sort an array using bubble sort.cpp

--- cpp to sort an array using bubble sort.cpp
+++ cpp to sort an array using bubble sort.cpp
@@ -3,29 +3,21 @@ using namespace std;
 
 int main() {
     int n;
-    cout << "Enter the number of elements in the array: ";
+    cout << "Enter number of elements: ";
     cin >> n;
+    int arr[n];
 
-    int arr[100];
-    cout << "Enter the elements:\n";
-    for (int i = 0; i < n; i++) {
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++)
         cin >> arr[i];
-    }
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
 
-    cout << "\nArray after Bubble Sort:\n";
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
-    }
-    cout << endl;
-
     return 0;
 }
